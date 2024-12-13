42f8dc38ae753a0d0903cd5e5d598a0c0f1d5f41
Kennguyen230 <kennguyen230@gmail.com>
2024-12-13 16:13:16 -0500

    Added layouts directory to tailwindconfigjs

--- client/src/layouts/HamburgerMenu.tsx
+++ client/src/layouts/HamburgerMenu.tsx
@@ -52,7 +52,7 @@ export function HamburgerMenu() {
       {/* The RHS sheet menu */}
       <Sheet>
         <SheetTrigger asChild>
-          <Button className="bg-[#11161D] hover:rounded-lg hover:bg-slate-900/90">
+          <Button className="bg-[#11161D] hover:rounded-lg hover:bg-[#182B40]">
             <i
               className="fa-solid fa-bars fa-xl"
               style={{ color: "#ffffff" }}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,7 @@ export const content = [
   './components/**/*.{ts,tsx}',
   './app/**/*.{ts,tsx}',
   './src/**/*.{ts,tsx}',
+  './src/layouts/*.{ts,tsx}'
 ];
 export const theme = {
   container: {
