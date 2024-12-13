fef55f375fd86b4ec05b337b7b1097cfc1669b49
Kennguyen230 <kennguyen230@gmail.com>
2024-12-13 14:48:41 -0500

    Removed redundant client/client directory and moved merged global.css+tailwind.config.js files. Pointed components.json config and css fields to correct places

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         'vollkorn': ['Vollkorn SC', 'sans-serif'],
